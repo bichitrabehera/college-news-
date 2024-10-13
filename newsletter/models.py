240610635ea20ba4cd1ad3a9398a4ccf0ab6adf2
Masaru124 <masarukaze041@gmail.com>
2024-10-13 19:56:40 +0530

    submit article

--- newsletter/models.py
+++ newsletter/models.py
@@ -4,6 +4,8 @@ class Article(models.Model):
     title = models.CharField(max_length=200)
     content = models.TextField()
     created_at = models.DateTimeField(auto_now_add=True)
+    is_approved = models.BooleanField(default=False)
+    is_published = models.BooleanField(default=False)  # Add this line
 
     def __str__(self):
         return self.title
--- newsletter/views.py
+++ newsletter/views.py
@@ -1,6 +1,37 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from .models import Article
+from .forms import ArticleForm  
+
 
 def home(request):
-    articles = Article.objects.all().order_by('-created_at')
+    articles = Article.objects.filter(is_approved=True)  # Only show approved articles
     return render(request, 'newsletter/home.html', {'articles': articles})
+
+
+
+def submit_article(request):
+    if request.method == 'POST':
+        form = ArticleForm(request.POST)
+        if form.is_valid():
+            article = form.save(commit=False)
+            article.is_approved = False  # Set to False initially
+            article.save()
+            return redirect('home')  # Redirect to home after submission
+    else:
+        form = ArticleForm()
+    return render(request, 'newsletter/submit_article.html', {'form': form})
+
+def approve_article(request, article_id):
+    article = Article.objects.get(id=article_id)
+    article.is_approved = True  # Approve the article
+    article.save()
+    return redirect('admin_article_list')  # Redirect to admin list after approval
+
+def admin_article_list(request):
+    articles = Article.objects.filter(is_approved=False)  # List unapproved articles
+    return render(request, 'newsletter/admin_article_list.html', {'articles': articles})
+def approve_article(request, article_id):
+    article = get_object_or_404(Article, id=article_id)
+    article.is_approved = True
+    article.save()
+    return redirect('home')
