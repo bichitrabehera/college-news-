5803314bbf450d477ae5829eff9fa48325f384a2
Masaru124 <masarukaze041@gmail.com>
2024-12-06 20:41:55 +0530

    Normalize line endings in staticfiles/style.css to LF

--- news/settings.py
+++ news/settings.py
@@ -13,7 +13,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = env('uoxh$1f@bi(_%6dwv1b_)x^yl*mm_(ba_i-o62zi9xo_xc67h#')
+SECRET_KEY = 'django-insecure-8!9^*1234567890abcdef1234567890abcdef1234567890abcdef1234567890'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = env.bool('DEBUG', default=False)
