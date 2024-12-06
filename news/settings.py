ec53f74a8a6d30a42b49dc48cacfbda4307da3a7
Masaru124 <masarukaze041@gmail.com>
2024-12-06 20:35:59 +0530

    Normalize line endings in news/settings.py to LF

--- news/settings.py
+++ news/settings.py
@@ -13,7 +13,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = env('SECRET_KEY')
+SECRET_KEY = env('uoxh$1f@bi(_%6dwv1b_)x^yl*mm_(ba_i-o62zi9xo_xc67h#')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = env.bool('DEBUG', default=False)
