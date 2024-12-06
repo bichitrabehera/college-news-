0d63c76c48efbb767e0cb57cd57da86ac2a3a73f
Masaru124 <masarukaze041@gmail.com>
2024-12-06 21:20:52 +0530

    p6

--- news/settings.py
+++ news/settings.py
@@ -19,7 +19,9 @@ SECRET_KEY = env('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = env.bool('DEBUG', default=False)
 
-ALLOWED_HOSTS = env.list('ALLOWED_HOSTS', default=['college-news.vercel.app'])
+import sys
+ALLOWED_HOSTS = ['college-news.vercel.app', '127.0.0.1']
+sys.stdout.write(f"ALLOWED_HOSTS: {ALLOWED_HOSTS}\n")
 
 # Application definition
 
