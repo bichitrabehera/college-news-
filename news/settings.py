ab52653ca171972435a0d326861237627e3eecbb
Masaru124 <masarukaze041@gmail.com>
2024-12-06 20:28:37 +0530

    Update ALLOWED_HOSTS for Vercel deployment

--- news/settings.py
+++ news/settings.py
@@ -18,7 +18,7 @@ SECRET_KEY = env('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = env.bool('DEBUG', default=False)
 
-ALLOWED_HOSTS = env.list('ALLOWED_HOSTS', default=['*'])
+ALLOWED_HOSTS = env.list('ALLOWED_HOSTS', default=['college-news.vercel.app'])
 
 # Application definition
 
