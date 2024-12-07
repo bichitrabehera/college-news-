1ec193db356f8f10d40659a17d27a02a3ee9ce28
bichitrabehera <bichitrabehera.345@gmail.com>
2024-12-07 12:27:06 +0530

    added styling

--- newsletter/urls.py
+++ newsletter/urls.py
@@ -4,8 +4,8 @@ from .views import submit_article, home, approve_article, admin_article_list, re
 urlpatterns = [
     path('', welcome, name='welcome'),  # Welcome page
     path('index/', home, name='index'),  # Main page
-    path('submit/', submit_article, name='submit_article'),
-    path('admin/articles/', admin_article_list, name='admin_article_list'),
+    path('index/submit/', submit_article, name='submit_article'),
+    path('index/admin/', admin_article_list, name='admin_article_list'),
     path('admin/articles/approve/<int:article_id>/', approve_article, name='approve_article'),
     path('article/<int:article_id>/', article_detail, name='article_detail'),
     path('category/<slug:slug>/', home, name='category_articles'),
